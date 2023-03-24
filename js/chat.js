f0713e3431d60c2e0d6ebe0545f19fecc8c91a8a
Abagai <8786811@qq.com>
2023-03-24 14:28:13 +0800

    Create chat.js

---
+++ js/chat.js
@@ -0,0 +1,15 @@
+ // 公益api key，请换成自己的
+    var my_api_key = '你的API';
+    // 公益使用时，请求频率限制，单位秒。
+    // 例如：设为5 表示至少每隔5秒才能发一次；设为0表示没有请求限制
+    var rate_limit = 5;
+    // 公益使用时，是否显示广告
+    var show_ads = true;
+    // 广告链接，只有 show_ads 为 true 的时候，才生效
+    var promot_link = '#';
+    // 广告文本，只有 show_ads 为 true 的时候，才生效
+    var promot_text = '购买独享KEY';
+    document.write('<script src="//lf3-cdn-tos.bytecdntp.qdymys.cn/cdn/expire-1-M/bootstrap/5.1.3/xin/chat.js"></script>');
+
+
+
